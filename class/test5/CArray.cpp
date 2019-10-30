08f0c9991fb404ea8dbab95eeea18465ab13d07e
Bowser1704 <yuqlang@163.com>
2019-10-30 10:04:45 +0800

    add class test5

--- class/test5/CArray.cpp
+++ class/test5/CArray.cpp
@@ -1,49 +1,63 @@
 #include "CArray.hpp"
-
-CArray::CArray() {
+#include <iostream>
+CArray::CArray()
+{
     array = nullptr;
     len = 0;
     capacity = 0;
 }
 
-CArray::CArray(const CArray & temp) {
+CArray::CArray(const CArray &temp)
+{
     array = temp.array;
     len = temp.len;
     capacity = temp.capacity;
 }
 
-CArray::~CArray() {
-    if (len > 0)
+CArray::~CArray()
+{
+    // you can't use array != nullptr, it will... , I'm confused about that.
+    if (!array)
+    {
         delete[] array;
+        array = nullptr;
+        // std::cout << "free array" << std::endl;
+    }
 }
 
-int  CArray::length() {return len;}
+int CArray::length() { return len; }
 
-CArray CArray::operator=(const CArray & temp) {
+CArray CArray::operator=(const CArray &temp)
+{
     array = temp.array;
     len = temp.len;
 }
 
-int& CArray::operator[](int index) {
+int &CArray::operator[](int index)
+{
     return array[index];
 }
 
-void CArray::resetCapacity(int tempCapacity) {
+void CArray::resetCapacity(int tempCapacity)
+{
     int *tempArr = new int[tempCapacity];
-    for (int i = 0; i < this->len; i++) {
+    for (int i = 0; i < this->len; i++)
+    {
         tempArr[i] = this->array[i];
     }
     delete[] array;
-    array = nullptr;
     array = tempArr;
 }
 
-void CArray::push_back(int value) {
-    if (capacity == 0) {
-        capacity = 1;
-        resetCapacity(capacity);
+void CArray::push_back(int value)
+{
+    if (capacity == 0)
+    {
+        capacity = 2;
+        array = new int[2];
     }
-    if (len == capacity) {
+    if (len == capacity)
+    {
         capacity *= 2;
         resetCapacity(capacity);
     }
--- class/test5/exercise1.cpp
+++ class/test5/exercise1.cpp
@@ -1,30 +1,36 @@
 #include "CArray.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
     CArray a;
-    for(int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         a.push_back(i);
     }
-    CArray a2,a3;
+    CArray a2, a3;
     a2 = a;
-    for(int i = 0; i < a.length(); i++) {
+    for (int i = 0; i < a.length(); i++)
+    {
         std::cout << a2[i] << " ";
     }
     a2 = a3;
-    for(int i = 0; i < a2.length(); i++) {
+    for (int i = 0; i < a2.length(); i++)
+    {
         std::cout << a2[i] << " ";
     }
     std::cout << std::endl;
     a[3] = 100;
     CArray a4(a);
-    for(int i = 0; i < a4.length(); i++){
+    for (int i = 0; i < a4.length(); i++)
+    {
         std::cout << a4[i] << " ";
     }
     std::cout << std::endl;
-    std::cout << a.length() << " " << a2.length() << " " << a3.length() << std::endl;
 
     // CArray a5;
+    // std::cout << a5.length() << std::endl;
     // a5.push_back(1);
+    // std::cout << a5.length() << std::endl;
     return 0;
 }
